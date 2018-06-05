3271eef84dcee846b3f71952225ce7ee29c5df14
Tomo Eguchi <tomo.eguchi@noaa.gov>
2018-06-04 17:29:37 -0700

    New notebook file

---
+++ HILL_BiOp_functions.R
@@ -0,0 +1,30 @@
+#Dc_Indonesia_nesting_fcns
+
+# these are functions that are used in this project.
+# TomosFunctions.R is needed also.
+
+#sysInfo <- Sys.info()
+ifelse(Sys.info()[1] == 'Linux',
+       source('~/Documents/R/tools/TomosFunctions.R'),
+       source('~/R/tools/TomosFunctions.R'))
+
+library(ggplot2)
+library(tidyverse)
+library(lubridate)
+
+save.fig <- F
+
+sum.posterior <- function(yr, months = c(1:12), Xs.stats, zm) {
+  Xs.stats %>%
+    mutate(var.name = rownames(Xs.stats)) %>%
+    filter(year == yr) %>%
+    filter(month %in% months) %>%
+    select(var.name) -> Xs.name
+  zm.yr <- apply(Xs.name,
+                 MARGIN = 1,
+                 FUN = extract.samples,
+                 zm)
+
+  return(list(samples = zm.yr, var.names = Xs.name))
+}
+
