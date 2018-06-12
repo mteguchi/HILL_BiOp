d01e08810c181b94b2d2ea0d8649fb9dc582ca07
Tomo Eguchi <tomo.eguchi@noaa.gov>
2018-06-11 17:35:58 -0700

    UPdated analysis and html docs

---
+++ time_series_W_SSAR1_month_2006_June2018.R
@@ -0,0 +1,184 @@
+#time series analysis
+
+
+rm(list=ls())
+
+tic <- Sys.time()
+Sys <- Sys.info()
+
+source('HILL_BiOp_functions.R')
+library(rjags)
+library(bayesplot)
+
+save.RData <- T
+save.fig <- F
+
+MCMC.params <- list(n.chains = 3,
+                    n.iter = 50000,
+                    n.adapt = 100000)
+# get JM data first:
+data.0.W <- read.csv('data/W_nests.csv')
+# create regularly spaced time series:
+data.2.W <- data.frame(Year = rep(min(data.0.W$Year_begin,
+                                      na.rm = T):max(data.0.W$Year_begin,
+                                                     na.rm = T),
+                                  each = 12),
+                       Month_begin = rep(1:12,
+                                         max(data.0.W$Year_begin,
+                                             na.rm = T) -
+                                           min(data.0.W$Year_begin,
+                                               na.rm = T) + 1)) %>%
+  mutate(begin_date = as.Date(paste(Year,
+                                    Month_begin,
+                                    '01', sep = "-"),
+                              format = "%Y-%m-%d"),
+         Frac.Year = Year + (Month_begin-0.5)/12) %>%
+  select(Year, Month_begin, begin_date, Frac.Year)
+
+data.0.W %>% mutate(begin_date = as.Date(paste(Year_begin,
+                                                Month_begin,
+                                                '01', sep = "-"),
+                                          format = "%Y-%m-%d")) %>%
+  mutate(Year = Year_begin,
+         Month = Month_begin,
+         f_month = as.factor(Month),
+         f_year = as.factor(Year),
+         Frac.Year = Year + (Month_begin-0.5)/12,
+         Nests = W.1) %>%
+  select(Year, Month, Frac.Year, begin_date, Nests) %>%
+  na.omit() %>%
+  right_join(.,data.2.W, by = "begin_date") %>%
+  transmute(Year = Year.y,
+            Month = Month_begin,
+            Frac.Year = Frac.Year.y,
+            Nests = Nests) %>%
+  reshape::sort_df(.,vars = "Frac.Year") %>% #-> data.1.W
+  filter(Year > 2005) -> data.1.W
+
+bugs.data <- list(y = data.1.W$Nests,
+                  m = data.1.W$Month,
+                  T = nrow(data.1.W))
+
+load.module('dic')
+params <- c('theta', 'sigma.pro1', 'sigma.pro2',
+            'sigma.obs')
+
+jm <- jags.model(file = 'models/model_SSAR1_month_Warmon.txt',
+                 data = bugs.data,
+                 #inits = inits.function,
+                 n.chains = MCMC.params$n.chains,
+                 n.adapt = MCMC.params$n.adapt)
+
+# check for convergence first.
+zm <- coda.samples(jm,
+                   variable.names = params,
+                   n.iter = MCMC.params$n.iter)
+g.diag <- gelman.diag(zm)
+
+# plot posterior densities using bayesplot functions:
+# mcmc_dens(zm, 'theta')
+# mcmc_dens(zm, 'sigma.pro')
+# mcmc_dens(zm, 'sigma.obs')
+
+# then sample y
+params <- c(params, 'y', 'X', 'deviance')
+zm <- coda.samples(jm,
+                   variable.names = params,
+                   n.iter = MCMC.params$n.iter)
+
+summary.zm <- summary(zm)
+
+# extract ys
+ys.stats <- data.frame(summary.zm$quantiles[grep(pattern = 'y[/[]',
+                                                 row.names(summary.zm$quantiles)),
+                                            c('2.5%', '50%', '97.5%')])
+colnames(ys.stats) <- c('low_y', 'median_y', 'high_y')
+ys.stats$time <- data.1.W$Frac.Year
+ys.stats$obsY <- data.1.W$Nests
+ys.stats$month <- data.1.W$Month
+ys.stats$year <- data.1.W$Year
+
+# extract Xs - the state model
+Xs.stats <- data.frame(summary.zm$quantiles[grep(pattern = 'X[/[]',
+                                                 row.names(summary.zm$quantiles)),
+                                            c('2.5%', '50%', '97.5%')])
+colnames(Xs.stats) <- c('low_X', 'median_X', 'high_X')
+Xs.stats$time <- data.1.W$Frac.Year
+Xs.stats$obsY <- data.1.W$Nests
+Xs.stats$month <- data.1.W$Month
+Xs.stats$year <- data.1.W$Year
+
+Xs.year <- group_by(Xs.stats, year) %>% summarize(mode = sum(median_X),
+                                                  low = sum(low_X),
+                                                  high = sum(high_X))
+
+toc <- Sys.time()
+dif.time <- toc - tic
+p.1 <- ggplot() +
+  #geom_point(data = ys.stats,
+  #           aes(x = time, y = mode_y), color = "blue") +
+  #geom_line(data = Xs.stats,
+  #          aes(x = time, y = mode_X), color = 'blue') +
+  geom_line(data = Xs.stats,
+            aes(x = time, y = high_X), color = "red",
+            linetype = 2) +
+  geom_line(data = Xs.stats,
+            aes(x = time, y = low_X), color = "red",
+            linetype = 2) +
+  geom_point(data = Xs.stats,
+             aes(x = time, y = median_X), color = "red",
+             alpha = 0.5) +
+  geom_line(data = Xs.stats,
+            aes(x = time, y = median_X), color = "red",
+            alpha = 0.5) +
+  geom_point(data = ys.stats,
+             aes(x = time, y = obsY), color = "green",
+             alpha = 0.5)+
+  geom_line(data = ys.stats,
+             aes(x = time, y = obsY), color = "green",
+             alpha = 0.5) +
+  labs(x = '', y = '# nests')  +
+  theme(axis.text = element_text(size = 12),
+        text = element_text(size = 12))
+
+
+# results.Warmon_SSAR1_month_To2013 <- list(data.1 = data.1.W,
+#                                        bugs.data = bugs.data,
+#                                        summary.zm = summary.zm,
+#                                        Xs.stats = Xs.stats,
+#                                        Xs.year = Xs.year,
+#                                        ys.stats = ys.stats,
+#                                        zm = zm,
+#                                        tic = tic,
+#                                        toc = toc,
+#                                        dif.time = dif.time,
+#                                        Sys = Sys,
+#                                        MCMC.params = MCMC.params,
+#                                        g.diag = g.diag,
+#                                        jm = jm)
+
+results.Warmon_SSAR1_month_2006 <- list(data.1 = data.1.W,
+                                       bugs.data = bugs.data,
+                                       summary.zm = summary.zm,
+                                       Xs.stats = Xs.stats,
+                                       Xs.year = Xs.year,
+                                       ys.stats = ys.stats,
+                                       zm = zm,
+                                       tic = tic,
+                                       toc = toc,
+                                       dif.time = dif.time,
+                                       Sys = Sys,
+                                       MCMC.params = MCMC.params,
+                                       g.diag = g.diag,
+                                       jm = jm)
+
+if (save.fig)
+  ggsave(plot = p.1,
+         filename = 'figures/predicted_counts_SSAR1_month_Warmon_2006.png',
+         dpi = 600)
+
+if (save.RData)
+  #save(results.Warmon_SSAR1_month_To2013,
+  #     file = paste0('RData/SSAR1_month_W_all_', Sys.Date(), '.RData'))
+  save(results.Warmon_SSAR1_month_2006,
+       file = paste0('RData/SSAR1_month_W_2006_', Sys.Date(), '.RData'))
--- time_series_W_SSAR1_month_var_theta.R
+++ time_series_W_SSAR1_month_var_theta.R
@@ -5,9 +5,9 @@ rm(list=ls())
 
 tic <- Sys.time()
 Sys <- Sys.info()
-source('Dc_Indonesia_nesting_fcns.R')
+source('HILL_BiOp_functions.R')
 library(rjags)
-library(jagsUI)
+#library(jagsUI)
 
 save.RData <- T
 save.fig <- F
@@ -16,7 +16,7 @@ MCMC.params <- list(n.chains = 3,
                     n.iter = 50000,
                     n.adapt = 100000)
 
-data.0 <- read.csv("data/NestCounts_Warmon_27March2018.csv")
+data.0 <- read.csv("data/NestCounts_Warmon_30May2018.csv")
 
 # create time-duration filed (in yrs)
 # define dates with begin and end dates:
@@ -27,9 +27,9 @@ data.1$MONTH <- unlist(lapply(data.1$month, FUN = mmm2month))
 
 data.2 <- mutate(data.1, f.month = as.factor(MONTH),
                  f.year = as.factor(YEAR))%>%
-  filter(YEAR < 2014 & YEAR > 2005) %>%
+  #filter(YEAR < 2014 & YEAR > 2005) %>%
   #filter(YEAR > 2005) %>%
-  #filter(YEAR < 2014) %>%
+  filter(YEAR < 2014) %>%
   mutate(Frac.Year = YEAR + (MONTH-0.5)/12) %>%
   reshape::sort_df(.,vars = "Frac.Year")
 
@@ -46,7 +46,7 @@ load.module("dic")
 load.module("glm")
 # autojags from jagsUI seems to work pretty good - can't sample missing data
 # points because of the same reason as before - convergence cannot be determined.
-# parallel runs don't seem to work... 
+# parallel runs don't seem to work...
 # autojags.out <- autojags(data = bugs.data,
 #                          inits = NULL,
 #                          parameters.to.save = params,
@@ -90,23 +90,23 @@ summary.zm <- summary(zm)
 
 # Computing WAIC. Code is from
 # https://sourceforge.net/p/mcmc-jags/discussion/610036/thread/8211df61/
-WAIC.s <- jags.samples(jm, c("deviance", "WAIC"), 
-                       type="mean", 
-                       n.iter=10000, thin=10)
-WAIC.s <- lapply(WAIC.s, unclass)
-sapply(WAIC.s, sum)
+# WAIC.s <- jags.samples(jm, c("deviance", "WAIC"),
+#                        type="mean",
+#                        n.iter=10000, thin=10)
+# WAIC.s <- lapply(WAIC.s, unclass)
+# sapply(WAIC.s, sum)
 
-# According to the website, "the monitor called WAIC does 
+# According to the website, "the monitor called WAIC does
 # indeed return the WAIC penalty, so it is currently misnamed."  So that means
-# we need to convert what it comes back to the actual WAIC.  Alternatively, 
-# according to Matt Denwood: "In JAGS - I have been playing with the DIC module 
-# (locally but I will push to the repo). I think the easiest approach is to set 
-# monitors for elpd_waic and p_waic (named for consistency with the loo package) 
-# and calculate waic from this in R. To be honest WAIC was the main driver behind 
-# me adding the running variance monitor to JAGS last year but I never quite got 
+# we need to convert what it comes back to the actual WAIC.  Alternatively,
+# according to Matt Denwood: "In JAGS - I have been playing with the DIC module
+# (locally but I will push to the repo). I think the easiest approach is to set
+# monitors for elpd_waic and p_waic (named for consistency with the loo package)
+# and calculate waic from this in R. To be honest WAIC was the main driver behind
+# me adding the running variance monitor to JAGS last year but I never quite got
 # around to the next step before JAGS 4.3.0..."  But the folllowing code does
-# not workj - elpd_waic and p_waic cannot be found...  
-# waic <- jags.samples(jm, c("elpd_waic", "p_waic"), 
+# not workj - elpd_waic and p_waic cannot be found...
+# waic <- jags.samples(jm, c("elpd_waic", "p_waic"),
 #                      type = "mean",
 #                      n.iter=10000, thin=10)
 
@@ -115,7 +115,7 @@ sapply(WAIC.s, sum)
 ys.stats <- data.frame(summary.zm$quantiles[grep(pattern = 'y[/[]',
                                                  row.names(summary.zm$quantiles)),
                                             c('2.5%', '50%', '97.5%')])
-colnames(ys.stats) <- c('low_y', 'mode_y', 'high_y')
+colnames(ys.stats) <- c('low_y', 'median_y', 'high_y')
 ys.stats$time <- data.2$Frac.Year
 ys.stats$obsY <- data.2$count
 ys.stats$month <- data.2$MONTH
@@ -125,13 +125,13 @@ ys.stats$year <- data.2$YEAR
 Xs.stats <- data.frame(summary.zm$quantiles[grep(pattern = 'X[/[]',
                                                  row.names(summary.zm$quantiles)),
                                             c('2.5%', '50%', '97.5%')])
-colnames(Xs.stats) <- c('low_X', 'mode_X', 'high_X')
+colnames(Xs.stats) <- c('low_X', 'median_X', 'high_X')
 Xs.stats$time <- data.2$Frac.Year
 Xs.stats$obsY <- data.2$count
 Xs.stats$month <- data.2$MONTH
 Xs.stats$year <- data.2$YEAR
 
-Xs.year <- group_by(Xs.stats, year) %>% summarize(mode = sum(mode_X),
+Xs.year <- group_by(Xs.stats, year) %>% summarize(median = sum(median_X),
                                                   low = sum(low_X),
                                                   high = sum(high_X))
 
@@ -144,10 +144,10 @@ p.1 <- ggplot() +
             aes(x = time, y = high_X), color = "red",
             linetype = 2) +
   geom_point(data = Xs.stats,
-             aes(x = time, y = mode_X), color = "red",
+             aes(x = time, y = median_X), color = "red",
              alpha = 0.5, size = 2) +
   geom_line(data = Xs.stats,
-            aes(x = time, y = mode_X), color = "red",
+            aes(x = time, y = median_X), color = "red",
             alpha = 0.5) +
   geom_line(data = Xs.stats,
             aes(x = time, y = low_X), color = "red",
@@ -173,16 +173,15 @@ results.W_SSAR1_month_var_theta <- list(data.1 = data.2,
                                         Sys = Sys,
                                         MCMC.params = MCMC.params,
                                         g.diag = g.diag,
-                                        jm = jm,
-                                        WAIC.penalty = WAIC.s)
+                                        jm = jm)
 if (save.fig)
   ggsave(plot = p.1,
-         filename = 'figures/predicted_counts_W_month_var_theta_2006To2013.png',
+         filename = 'figures/predicted_counts_W_month_var_theta_To2013.png',
          height = 6, width = 8, units = "in", dpi = 600)
 
 if (save.RData)
   save(results.W_SSAR1_month_var_theta,
-       file = paste0('RData/SSAR1_month_W_var_theta_2006To2013_',
+       file = paste0('RData/SSAR1_month_W_var_theta_To2013_',
                      Sys.Date(), '.RData'))
 
 
