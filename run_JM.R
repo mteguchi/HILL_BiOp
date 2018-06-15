ffe0e3cbed8226cdc149d19ff45634ad031cd983
Tomo Eguchi <tomo.eguchi@noaa.gov>
2018-06-14 23:58:30 -0700

    JM updated analyses

---
+++ run_JM.R
@@ -0,0 +1,3 @@
+source('time_series_JM_SSAR1_month_var_theta_1999.R')
+source('time_series_JM_SSAR1_month_var_1999.R')
+source('time_series_JM_SSAR1_month_1999.R')
---
+++ run_v2.R
@@ -0,0 +1,3 @@
+source('time_series_W_SSAR1_month_var_theta_v2_June2018.R')
+source('time_series_W_SSAR1_month_2004_v2_June2018.R')
+source('time_series_W_SSAR1_month_2006_v2_June2018.R')
