b690ca5d515f45c7b300d0d5efb91d978d6450f1
Tomo Eguchi <Tomo.Eguchi@noaa.gov>
2018-10-01 17:16:45 -0700

    some lines deleted after conflict

--- HILL_BiOp_functions.R
+++ HILL_BiOp_functions.R
@@ -32,15 +32,13 @@ pareto.k.diag <- function(jm, MCMC.params, jags.data){
   
   n.per.chain <- (MCMC.params$n.samples - MCMC.params$n.burnin)/MCMC.params$n.thin
   
-<<<<<<< HEAD
   loglik.obs <- jm$sims.list$loglik[, !is.na(jags.data$y)]
   # get rid of NA columns - even if data existed (for example the first value) - no likelihood
   # for the first data point
   loglik.obs <- loglik.obs[, colSums(is.na(loglik.obs)) == 0]
-=======
-  loglik.obs <- jm$sims.list$loglik[, 2:jags.data$T]
+
+  #loglik.obs <- jm$sims.list$loglik[, 2:jags.data$T]
   
->>>>>>> 6bd6ecf3e1a4447aa2dd88f258ea4d68be8d5fa8
   Reff <- relative_eff(exp(loglik.obs), 
                        chain_id = rep(1:MCMC.params$n.chains, 
                                       each = n.per.chain))
